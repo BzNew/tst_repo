50115857492d7361b704404d4409116c40ac68e7
BzNew <mailonlydrive@gmail.com>
2023-11-02 23:57:03 +0300

    3 commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 int main(int, char**){
-    std::cout << "Hello, from cmake_vs_code!\n";
-    std::cout << "my string\n";
+  std::cout << R"(Hello, from cmake_vs_code!
+)";
+  std::cout << R"(my string
+)";
+
+  std::cout << "new string" << std::endl;
+
 
 }
